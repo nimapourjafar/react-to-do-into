8d53baeb91b01d74c3a34f2c49f2e495a4e1e471
Nima Pourjafar <nima.pourjafar123@gmail.com>
2021-05-04 16:46:56 -0400

    add click stuff

--- react-task-tracker/src/App.js
+++ react-task-tracker/src/App.js
@@ -1,9 +1,32 @@
 import Header from './components/Header'
+import Tasks from './components/Tasks'
+import {useState} from 'react'
 
 function App() {
+
+  const [tasks, setTasks] = useState(
+    [
+        {
+            id:1,
+            text:"Do Homework",
+            day: "Feb 5th at 2:30pm",
+            reminder:false               
+        }
+    ] 
+ )
+
+ const deleteTask = (id) =>{
+   setTasks(tasks.filter( (task) =>task.id !==id ))
+ }
+
+ const toggleReminder = (id) =>{
+   console.log(id)
+ }
+
   return (
     <div className="container">
       <Header name='Nima' />
+      {tasks.length >0 ? <Tasks tasks = {tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : "No More Tasks"}
     </div>
   );
 }
